3a14b535b6684ba96151b5a574d8237fd50e085e
Tim Spencer <72101647+sei-tspencer@users.noreply.github.com>
2025-06-17 12:50:10 -0400

    telemetry working (#51)
    * telemetry working
    
    * eliminate manual new-ing of TelemetryService and reduce time before renewing cache
    
    * remove unused file

---
+++ src/Player.Vm.Api/Domain/Services/TelemetryService.cs
@@ -0,0 +1,26 @@
+// Copyright 2025 Carnegie Mellon University. All Rights Reserved.
+// Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
+
+using System.Diagnostics.Metrics;
+
+namespace Player.Vm.Api.Domain.Services
+{
+    public interface ITelemetryService
+    {
+    }
+
+    public class TelemetryService : ITelemetryService
+    {
+        public const string VmConsolesMeterName = "cmu_sei_player_vm_consoles";
+        public  readonly Meter VmConsolesMeter = new Meter(VmConsolesMeterName, "1.0");
+        public  Gauge<int> PlayerViewActiveConsoles;
+        public Counter<int> ConsoleAccessCounter;
+
+        public TelemetryService()
+        {
+            PlayerViewActiveConsoles = VmConsolesMeter.CreateGauge<int>("player_view_active_consoles");
+            ConsoleAccessCounter = VmConsolesMeter.CreateCounter<int>("console_access_counter");
+        }
+
+    }
+}
