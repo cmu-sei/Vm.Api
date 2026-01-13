e9de2c2359976743664641d353b483444a180b0e
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2026-01-13 17:00:28 +0000

    fix EventTransactionInterceptor

--- src/Player.Vm.Api/Data/VmContext.cs
+++ src/Player.Vm.Api/Data/VmContext.cs
@@ -2,7 +2,12 @@
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Player.Vm.Api.Domain.Models;
 using Player.Vm.Api.Infrastructure.Extensions;
 
@@ -12,6 +17,9 @@ namespace Player.Vm.Api.Data
     {
         // Needed for EventInterceptor
         public IServiceProvider ServiceProvider;
+
+        // Entity Events collected by EventTransactionInterceptor and published in SaveChanges
+        public List<INotification> Events { get; } = [];
 
         public VmContext(DbContextOptions options)
             : base(options) { }
@@ -32,6 +40,36 @@ namespace Player.Vm.Api.Data
             {
                 modelBuilder.AddPostgresUUIDGeneration();
                 modelBuilder.UsePostgresCasing();
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            var result = base.SaveChanges();
+            PublishEvents().Wait();
+            return result;
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+            await PublishEvents(cancellationToken);
+            return result;
+        }
+
+        private async Task PublishEvents(CancellationToken cancellationToken = default)
+        {
+            // Publish deferred events after transaction is committed and cleared
+            if (Events.Count > 0 && ServiceProvider is not null)
+            {
+                var mediator = ServiceProvider.GetRequiredService<IMediator>();
+                var eventsToPublish = Events.ToArray();
+                Events.Clear();
+
+                foreach (var evt in eventsToPublish)
+                {
+                    await mediator.Publish(evt, cancellationToken);
+                }
             }
         }
     }
